543697fda69e5cfc745f494834b304986b39595c
Yashwanth-2203 <245123751062@mvsrec.edu.in>
2026-03-09 15:31:28 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -1,27 +1,20 @@
-import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        double a, b;
-        char op;
-        
-        System.out.print("Enter first number: ");
-        a = sc.nextDouble();
-        
-        System.out.print("Enter operator (+ - * /): ");
-        op = sc.next().charAt(0);
-        
-        System.out.print("Enter second number: ");
-        b = sc.nextDouble();
-        
-        switch(op) {
-            case '+': System.out.println("Result = " + (a + b)); break;
-            case '-': System.out.println("Result = " + (a - b)); break;
-            case '*': System.out.println("Result = " + (a * b)); break;
-            case '/': System.out.println("Result = " + (a / b)); break;
-            default: System.out.println("Invalid operator");
-        }
+        // Inbuilt numbers
+        double num1 = 12;
+        double num2 = 4;
+
+        // Inbuilt operations
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num2 != 0 ? num1 / num2 : Double.NaN;
+
+        // Print results
+        System.out.println("Numbers: " + num1 + " and " + num2);
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
     }
 }
